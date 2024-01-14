711bbdfd2a8b15aef385cbc6d030ad7bad758216
Bright Jonathan <brightjonathan000@gmail.com>
2024-01-15 00:22:28 +0100

    working the layout

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
